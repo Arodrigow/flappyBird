4d520e168e577773b8b1f5dc83124a53a0628c7a
Arodrigow <rodrigow_@hotmail.com.br>
2020-06-29 21:52:37 -0300

    barrier generator working... kinda

--- js/flappy.js
+++ js/flappy.js
@@ -10,6 +10,7 @@ function Barrier(reverse = false){
 
     const border = newElement('div', 'border')
     const body = newElement('div', 'body')
+
     this.element.appendChild(reverse ? body : border)
     this.element.appendChild(reverse ? border : body)
 
@@ -34,13 +35,47 @@ function Barriers(height, space, x ){
         this.bottom.setHeight(heightBottom)
     }
 
-    this.getX = () => parseInt(this.element.style.left.split('rem')[0])
-    this.setX = () => this.element.style.left = `${x}rem`
-    this.getWidth = () => this.element.clientWidth
+    this.getX = () => parseFloat(this.element.style.left.split('rem')[0])
+    this.setX = x => this.element.style.left = `${x}rem`
+    this.getWidth = () => this.element.clientWitdh
 
     this.createSpace()
-    this.setX()
+    this.setX(x)
 }
 
-const b = new Barriers(25,5,40)
-document.querySelector('[wm-flappy]').appendChild(b.element)
+function barriersControl(height, width, space, off, pointNot){
+
+    this.pairs = [
+        new Barriers(height, space, width),
+        new Barriers(height, space, width + off),
+        new Barriers(height, space, width + off * 2),
+        new Barriers(height, space, width + off * 3),
+        new Barriers(height, space, width + off * 4)
+    ]
+
+    const deslc = 0.5
+
+    this.animate = () =>{
+        this.pairs.forEach(pair => {
+            pair.setX(pair.getX() - deslc)
+            console.log(pair.getX())
+            if (pair.getX() < -height){
+                pair.setX(pair.getX() + off * this.pairs.length)
+                pair.createSpace()
+            }
+
+           /* const mid = width / 2
+            const midCross = pair.getX() + deslc >= mid
+                && pair.getX() < mid
+
+            if (midCross) pointNot()*/
+        })
+    }
+}
+
+const barriers = new barriersControl(30, 60, 10, 20)
+const gameArea = document.querySelector('[wm-flappy]')
+barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+setInterval(() => {
+    barriers.animate()
+}, 5)
